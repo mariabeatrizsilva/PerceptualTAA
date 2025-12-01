c6d7a9ae4fd5cc72f697e4eb0de17846aca85d60
Maria Beatriz Silva <mariabeatrizfreiresilva@gmail.com>
2025-12-01 15:15:40 -0500

    added parkenv

--- compute_metrics.py
+++ compute_metrics.py
@@ -22,7 +22,7 @@ import re
 
 REF_NAME = '16SSAA'
 BASE_MP4 = 'data/'
-BASE_FRAMES = 'data/frames/'
+BASE_FRAMES = 'data/parkenv/'
 FRAMES_SUFFIX = '%04d.png'
 
 
@@ -220,9 +220,9 @@ def get_paths(folder_name: str, metric: Metric):
     frames_path = os.path.join(project_root, BASE_FRAMES, folder_name)
     
     if metric == Metric.CGVQM:
-        output_scores_path = os.path.join(project_root, 'outputs/scores_cgvqm_frames', score_file_name)
+        output_scores_path = os.path.join(project_root, 'outputs/parkenv/scores_cgvqm_frames', score_file_name)
     else:
-        output_scores_path = os.path.join(project_root, 'outputs/scores_cvvdp_new', score_file_name)
+        output_scores_path = os.path.join(project_root, 'outputs/parkenv/scores_cvvdp', score_file_name)
     
     return frames_path, output_scores_path
 
@@ -345,7 +345,7 @@ def compute_score_single(test_name: str, folder_path: str, ref_frames_folder: st
         
         # Create error map path for this video
         err_map_name = f"{test_name}_errmap.mp4"
-        err_map_path = os.path.join(project_root, 'outputs/err_maps_frames', err_map_name)
+        err_map_path = os.path.join(project_root, 'outputs/citypark/err_maps_frames', err_map_name)
         
         # Optional: if you still have MP4s and want to use them for visualization
         dist_video_path = os.path.join(project_root, BASE_MP4, folder_path.split('/')[-1], f"{test_name}.mp4")
